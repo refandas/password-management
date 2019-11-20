82f1cee3ccd522a24d147f19c435eb0e53c20990
refandas <refanda10ss@gmail.com>
2019-11-20 17:34:36 +0700

    limit menu acces based on access_level

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -16,49 +16,40 @@
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item has-treeview">
-                    <a href="<?= base_url('dashboard'); ?>" class="nav-link active">
+                    <a href="<?= base_url('dashboard'); ?>" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>
-                            Dashboard
-                        </p>
+                        <p>Dashboard</p>
                     </a>
                 </li>
 
-                <li class="nav-item mt-4">
+                <?php if ($this->session->userdata('access_level') == 1) : ?>
+                <li class="nav-item mt-1">
                     <a href="<?= base_url('dashboard/users'); ?>" class="nav-link">
                         <i class="nav-icon fas fa-user-friends"></i>
                         <p>USER</p>
                     </a>
                 </li>
+                <?php endif; ?>
 
+                <?php if ($this->session->userdata('access_level') == 2) : ?>
                 <li class="nav-item mt-4">
-                    <a href="https://adminlte.io/docs/3.0" class="nav-link">
+                    <a href="<?= base_url('account/index'); ?>" class="nav-link">
                         <i class="nav-icon fas fa-window-restore"></i>
-                        <p>SITE</p>
+                        <p>ACCOUNT</p>
                     </a>
                 </li>
-
-                <li class="nav-item mt-1">
-                    <a href="https://adminlte.io/docs/3.0" class="nav-link">
-                        <i class="nav-icon fas fa-wallet"></i>
-                        <p>WALLET</p>
-                    </a>
-                </li>
+                <?php endif; ?>
 
                 <li class="nav-item has-treeview mt-4">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-cogs"></i>
-                        <p>
-                            Setting
-                        </p>
+                        <p>Setting</p>
                     </a>
                 </li>
                 <li class="nav-item has-treeview">
                     <a href="<?= base_url('dashboard/logout'); ?>" class="nav-link">
                         <i class="nav-icon fas fa-sign-out-alt"></i>
-                        <p>
-                            Log Out
-                        </p>
+                        <p>Log Out</p>
                         </i>
                     </a>
                 </li>
